5b94ce679cf11c4f76c8404f08efd81ecc9d5b55
juhinahbatool-dotcom <juhinah.batool@nu.edu.pk>
2025-10-26 12:31:27 +0500

    app.py

--- app.py
+++ app.py
@@ -11,22 +11,22 @@ MODEL_DIR = "model"
 
 
 if not os.path.exists(MODEL_DIR):
-st.write("Downloading fine-tuned BERT model from Google Drive...")
-response = requests.get(DRIVE_MODEL_URL)
+  st.write("Downloading fine-tuned BERT model from Google Drive...")
+  response = requests.get(DRIVE_MODEL_URL)
 with open(MODEL_ZIP, "wb") as f:
-f.write(response.content)
-st.write("Download complete. Extracting model files...")
+  f.write(response.content)
+  st.write("Download complete. Extracting model files...")
 with zipfile.ZipFile(MODEL_ZIP, "r") as zip_ref:
-zip_ref.extractall(MODEL_DIR)
-st.success("Model extracted successfully!")
+  zip_ref.extractall(MODEL_DIR)
+  st.success("Model extracted successfully!")
 
 
 
 @st.cache_resource
 def load_model():
-model = BertForSequenceClassification.from_pretrained(MODEL_DIR)
-tokenizer = BertTokenizer.from_pretrained(MODEL_DIR)
-return model, tokenizer
+  model = BertForSequenceClassification.from_pretrained(MODEL_DIR)
+  tokenizer = BertTokenizer.from_pretrained(MODEL_DIR)
+  return model, tokenizer
 
 model, tokenizer = load_model()
 
@@ -42,12 +42,12 @@ st.markdown("Predict emotions like joy, sadness, anger, and neutral from any tex
 text = st.text_area("Enter your sentence:", height=120)
 
 if st.button("Predict Emotion"):
-if text.strip():
-# Tokenize and predict
-inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True, max_length=64)
-with torch.no_grad():
-logits = model(**inputs).logits
-pred = torch.argmax(logits, dim=1).item()
-st.success(f"**Predicted Emotion:** {emotion_labels[pred]}")
-else:
-st.warning("⚠️ Please enter a sentence to analyze.")
+  if text.strip():
+    # Tokenize and predict
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True, max_length=64)
+    with torch.no_grad():
+      logits = model(**inputs).logits
+      pred = torch.argmax(logits, dim=1).item()
+    st.success(f"**Predicted Emotion:** {emotion_labels[pred]}")
+  else:
+    st.warning("⚠️ Please enter a sentence to analyze.")
